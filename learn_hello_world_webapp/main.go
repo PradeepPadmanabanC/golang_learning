df88b31d3caf427614066eb03ef99b71b373ccf7
Pradeep Padmanaban C <pradeeppadmanabanc@gmail.com>
2025-05-13 23:41:57 +0530

    added webpages

--- learn_hello_world_webapp/main.go
+++ learn_hello_world_webapp/main.go
@@ -5,16 +5,28 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "this is the home page")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := AddValues(2, 2)
+
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("this is a about page and 2+2 is %d", sum))
+
+}
+
+func AddValues(x, y int) int {
+	return x + y
+}
+
 // test add
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello world!")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-	})
-
-	_ = http.ListenAndServe(":8080", nil)
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 }
